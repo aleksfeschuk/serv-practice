d02619152ffe60c641bc007dde29801edb300508
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-04-11 11:20:07 -0600

    new method middleware

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ let products = [
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    const timestamp = new Date().toISOString();
+    console.log(`[${timestamp}] ${req.method} ${req.url}`);
+    next();
+})
+
+
 app.get('/', (req, res) => {
     res.send('Hello it is my server!');
 });
